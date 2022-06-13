1e31cc2037e0a7194291d36f8110bf4f6e43b30b
Bmarx <b.marx981@gmail.com>
2022-06-13 07:09:20 -0400

    Add camera package and scaffold body

--- lib/ui/camera_page.dart
+++ lib/ui/camera_page.dart
@@ -8,6 +8,19 @@ class CameraPage extends StatefulWidget {
 }
 
 class _CameraPageState extends State<CameraPage> {
+  IconData getCameraLensIcon(CameraLensDirection direction) {
+    switch (direction) {
+      case CameraLensDirection.back:
+        return Icons.camera_rear;
+      case CameraLensDirection.front:
+        return Icons.camera_front;
+      case CameraLensDirection.external:
+        return Icons.camera;
+      default:
+        throw ArgumentError('Unknown lens direction');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
